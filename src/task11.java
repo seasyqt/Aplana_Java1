a6b1854c6dfd277198b7bc891c44ca9cdebbf6ef
seasyqt <kira_man@mail.ru>
2019-02-24 14:15:58 +0300

    Ввод строки с клавиатуры и вывод на экран

---
+++ src/task11.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class task11
+
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите любое слово : ");
+        String stroka = scan.next();
+        System.out.println("Полученная строка : ");
+        System.out.print(stroka);
+    }
+
+}
