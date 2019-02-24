d9b4fa8dcef4a45fa8be232b629539667c91fd53
seasyqt <kira_man@mail.ru>
2019-02-24 13:35:07 +0300

    Исправление с вычесление среднего арифмитического

--- src/task6.java
+++ src/task6.java
@@ -4,13 +4,13 @@ public class task6 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a,b,c;
+        double a,b,c;
         System.out.println("Введите 3 числа : ");
         a = scan.nextInt();
         b = scan.nextInt();
         c = scan.nextInt();
-        int result = (a+b+c)/3;
-        int sresult = result/2;
+        double result = (a+b+c)/3; //Либо можно было оставить объявление переменных с int , а тут переопределить 
+        double sresult = result/2;
 
         System.out.println("Среднее арифметическое число " + result);
         System.out.println("Деление " + result + " на 2 : " + sresult);
