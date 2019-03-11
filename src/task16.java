1479bbcfecf8534847904432a963a1b3c4149c03
seasyqt <kira_man@mail.ru>
2019-03-11 23:25:52 +0300

    Считывание текста с клавиатуры и запись в файл.Исправления

--- src/task16.java
+++ src/task16.java
@@ -1,13 +1,14 @@
 import java.io.*;
 
 public class task16 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        String s;
         try
         {
           //  File file = new File("test.txt");
             BufferedReader reader = new BufferedReader(new FileReader("test.txt"));
-            String s;
+
             while ((s = reader.readLine()) != null){
                 System.out.println(s);
             }
